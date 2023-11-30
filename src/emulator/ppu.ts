cfed7763b581fe2e808aee90f989e01867daa45f
David Rendell <davidwrendell@gmail.com>
2023-11-30 13:29:22 +0000

    Rendering background layer

--- src/emulator/ppu.ts
+++ src/emulator/ppu.ts
@@ -8,6 +8,10 @@ const COLOURS = [
   [0, 0, 0],
 ]
 
+const TILESET_BASE_ADDRESS = 0x8000
+
+type Tile = number[][]
+
 export default class PPU {
   cpu: CPU
   memory: Memory
@@ -15,6 +19,25 @@ export default class PPU {
   constructor(cpu: CPU) {
     this.cpu = cpu
     this.memory = cpu.memory
+  }
+
+  getTile(tileId: number): Tile {
+    const tileData: Tile = []
+    const tileBaseAddress = TILESET_BASE_ADDRESS + 16 * tileId
+    for (let row = 0; row < 8; row++) {
+      const rowData: number[] = []
+      const byte1 = this.memory.at(tileBaseAddress + 2 * row).read()
+      const byte2 = this.memory.at(tileBaseAddress + 2 * row + 1).read()
+      for (let bit = 7; bit >= 0; bit--) {
+        const bit1 = (byte1 >> bit) & 1
+        const bit2 = (byte2 >> bit) & 1
+        const pixelValue = bit1 + (bit2 << 1)
+
+        rowData.push(pixelValue)
+      }
+      tileData.push(rowData)
+    }
+    return tileData
   }
 
   printTileSet(canvas: HTMLCanvasElement): void {
@@ -57,7 +80,8 @@ export default class PPU {
     context.putImageData(imageData, 0, 0)
   }
 
-  printBackgroundLayer(canvas: HTMLCanvasElement, tiles: HTMLCanvasElement): void {
+  // Load BIOS to 0x0055 to test
+  printBackgroundLayer(canvas: HTMLCanvasElement): void {
     // Tilemap 1: 0x9800 - 0x9BFF, so 256 bytes
     // once byte is one tile surely?
     const context = canvas.getContext("2d")
@@ -65,5 +89,36 @@ export default class PPU {
     canvas.width = 256
     canvas.height = 256
 
+    const backgroundPalletByte = this.memory.at(0xFF47).read()
+
+    const pallet: number[][] = [
+      COLOURS[(backgroundPalletByte >> 0) & 2],
+      COLOURS[(backgroundPalletByte >> 2) & 2],
+      COLOURS[(backgroundPalletByte >> 4) & 2],
+      COLOURS[(backgroundPalletByte >> 6) & 2],
+    ]
+
+    for (let j = 0; j < 32; j++) {
+      const baseY = j * 8
+      for (let i = 0; i < 32; i++) {
+        const baseX = i * 8
+        const tileMapAddress = 0x9800 + i + (j * 32)
+        const tileNumber = this.memory.at(tileMapAddress).read()
+        const tileData = this.getTile(tileNumber)
+        const imageData = context.createImageData(8, 8)
+        for (let x = 0; x < 8; x++) {
+          for (let y = 0; y < 8; y++) {
+            const pixelNumber = (y * 8) + x
+            const pixelValue = tileData[y][x]
+            const colour = pallet[pixelValue]
+            imageData.data[4 * pixelNumber + 0] = colour[0]
+            imageData.data[4 * pixelNumber + 1] = colour[1]
+            imageData.data[4 * pixelNumber + 2] = colour[2]
+            imageData.data[4 * pixelNumber + 3] = 255
+          }
+        }
+        context.putImageData(imageData, baseX, baseY)
+      }
+    }
   }
 }
--- src/web/components/vramViewer.tsx
+++ src/web/components/vramViewer.tsx
@@ -6,21 +6,38 @@ interface Props {
 }
 
 export function VramViewer({ ppu }: Props) {
-  const canvasRef = React.useRef<HTMLCanvasElement>(null)
+  const tileSetCanvas = React.useRef<HTMLCanvasElement>(null)
+  const backgroundCanvas = React.useRef<HTMLCanvasElement>(null)
 
   const update = () => {
-    if (canvasRef.current) {
-      ppu.printTileSet(canvasRef.current)
+    if (tileSetCanvas.current) {
+      ppu.printTileSet(tileSetCanvas.current)
+    }
+    if (backgroundCanvas.current) {
+      ppu.printBackgroundLayer(backgroundCanvas.current)
     }
   }
 
   return (<section>
     <h2> VRAM Viewer</h2>
     <button onClick={update}>Update</button>
-    <canvas
-      width="128"
-      height="192"
-      ref={canvasRef}
-    />
+    <div className="flex-horizonally">
+      <div>
+        <h3>Tile data</h3>
+        <canvas
+          width="128"
+          height="192"
+          ref={tileSetCanvas}
+        />
+      </div>
+      <div>
+        <h3>Background layer</h3>
+        <canvas
+          width="256"
+          height="256"
+          ref={backgroundCanvas}
+        />
+      </div>
+    </div>
   </section>)
 }
