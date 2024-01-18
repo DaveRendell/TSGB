6983d8b7e3fb5c8b7ab5ecd9057f3d318521170b
David Rendell <davidwrendell@gmail.com>
2024-01-18 20:46:44 +0000

    Make PWA work I think

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -5,7 +5,23 @@ import {VitePWA} from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ injectRegister: "script" })
+    VitePWA({
+      injectRegister: "script",
+      manifest: {
+        "name": "TSGB - Game Boy Emulator",
+        "short_name": "TSGB",
+        "icons": [
+          {"src":"/TSGB/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},
+          {"src":"/TSGB/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}
+        ],
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+        "display": "standalone",
+        "description": "An emulator for game for the Game Boy console",
+        "start_url": "/TSGB/",
+        "lang": "en-gb"
+      }
+    })
   ],
   base: "/TSGB/",
   assetsInclude: "*.png",
