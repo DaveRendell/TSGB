ba34d5bebf3d9ee77ce7fab157f1f53c1f0ecc93
David Rendell <davidwrendell@gmail.com>
2024-07-31 08:29:20 +0100

    Printer: mock responses to console

--- src/emulator/serialConnections/printerConnection.ts
+++ src/emulator/serialConnections/printerConnection.ts
@@ -3,7 +3,16 @@ import { SerialConnection } from "./serialConnection";
 // https://gbdev.io/pandocs/Gameboy_Printer.html
 export class PrinterConnection implements SerialConnection {
   packetBuffer: number[] = []
-  status = 0x00
+
+  // Status bits
+  lowBattery = false
+  otherError = false
+  paperJam = false
+  packetError = false
+  unprocessedData = false
+  imageDataFull = false
+  printing = false
+  checksumError = false
 
   onReceiveByteFromConsole(byte: number): number {
     this.packetBuffer.push(byte)
@@ -11,12 +20,14 @@ export class PrinterConnection implements SerialConnection {
     if (!this.bufferIsValid()) {
       console.log("[PRINTER]: Invalid packet", [...this.packetBuffer])
       this.packetBuffer = []
+    } else if (this.packetBuffer.length === 9) {
+      return 0x81 // Magic keep alive signal
     } else if (this.bufferIsFinished()) {
       this.executeCommand()
+      return this.statusByte()
     }
-    // TODO check if buffer is complete
 
-    return this.status
+    return 0x00
   }
 
   private bufferIsValid(): boolean {
@@ -39,9 +50,69 @@ export class PrinterConnection implements SerialConnection {
 
   private executeCommand(): void {
     const commandId = this.packetBuffer[2]
+    const dataLength = this.packetBuffer[4] + (this.packetBuffer[5] << 8)
+    const data = this.packetBuffer.slice(6, 6 + dataLength)
+    const compressionFlag = this.packetBuffer[3] === 0x01
     
-    console.log(`[PRINTER]: Command 0x${commandId.toString(16).padStart(2, "0")} received`)
+    switch (commandId) {
+      case 0x01:
+        this.initialise()
+        break
+      case 0x02:
+        this.startPrinting(data)
+        break
+      case 0x04:
+        this.fillBuffer(data, compressionFlag)
+        break
+      case 0x0F:
+        this.nop()
+        break
+      default:
+        console.log(`[PRINTER] Received unknown command ID 0x${commandId.toString(16).padStart(2, "0")}`)
+    }
 
     this.packetBuffer = []
+  }
+
+  private initialise(): void {
+    console.log("[PRINTER] Received command: INITIALISE")
+
+    this.unprocessedData = false
+    this.packetError = false
+    this.checksumError = false
+    this.otherError = false
+  }
+
+  private startPrinting(data: number[]): void {
+    console.log("[PRINTER] Received command: START PRINTING")
+
+    this.printing = true
+    this.unprocessedData = false
+
+    const printer = this
+    setTimeout(() => {
+      printer.printing = false
+    }, 3000)
+  }
+
+  private fillBuffer(data: number[], compressed: boolean): void {
+    console.log("[PRINTER] Received command: FILL BUFFER")
+  }
+
+  private nop(): void {
+    console.log("[PRINTER] Received command: NOP")
+  }
+
+  private statusByte(): number {
+    return (
+      (this.lowBattery ? 0x80 : 0)
+      + (this.otherError ? 0x40 : 0)
+      + (this.paperJam ? 0x20 : 0)
+      + (this.packetError ? 0x10 : 0)
+      + (this.unprocessedData ? 0x08 : 0)
+      + (this.imageDataFull ? 0x04 : 0)
+      + (this.printing ? 0x02 : 0)
+      + (this.checksumError ? 0x01 : 0)
+    )
   }
 }
